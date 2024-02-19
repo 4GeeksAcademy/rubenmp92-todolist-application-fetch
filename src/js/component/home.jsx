941bc02d98964355bccbd148a5668654dc5fefb5
rubenmp92 <97123438+rubenmp92@users.noreply.github.com>
2024-02-19 20:30:54 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//logica 
+	const [todos, setTodos] = useState([])
+	//petición   resultado 	data 
+	//1º then (el estado si todo salio bien, etc)
+	//2º then (la info que llega (data))
+	//catch si algo sale mal en cuanto al código sale aquí
+	// GET si solo se pone la url en el fetch él asume que es un metodo GET
+
+	const urlTodos = "https://playground.4geeks.com/apis/fake/todos/user/rubenmp92"
+	fetch(urlTodos)
+	.then((response)=>{
+		return response.json()
+	}) //lo paso de json a javascript
+	.then((data)=>{setTodos(data)})
+	.catch((err)=>{return err})
+
+	//POST
+	//el fetch tiene toda la info de la petición
+	fetch(urlTodos, {
+		method: "POST",
+		body: JSON.stringify([]),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then((response)=>{return response.json()})
+	.then((data)=>{console.log(data)})
+	.catch((err)=>{return err})
+
+	//PUT
+	let body = [
+		{
+			done: false,
+			label: "barrer casa"
+		},
+		{
+			done: false,
+			label: "pasear perro"
+		},
+		{
+			done: false,
+			label: "lavar ropa"
+		}
+	]
+	fetch(urlTodos, {
+		method: "PUT",
+		body: JSON.stringify(body),
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+	.then((response)=>{return response.json()})
+	.then((data)=>{console.log(data)})
+	.catch((err)=>{return err})
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{todos.map((value)=>{
+				return<h3>{value.label}</h3>
+			})}
 		</div>
 	);
 };
